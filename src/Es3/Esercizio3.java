7c2f1221cf4af109533d59e8ad0c32a44a2de584
KeviN-XVII <kevinquartar@gmail.com>
2026-01-07 15:12:33 +0100

    Esercizio 3

--- src/Es3/Esercizio3.java
+++ src/Es3/Esercizio3.java
@@ -5,6 +5,18 @@ import java.util.Scanner;
 public class Esercizio3 {
     public static void main(String[] args) {
         Scanner scanner =new Scanner(System.in);
-        String frase = scanner.nextLine();
+        String frase = "";
+
+        while (!frase.equals(":q")){
+            System.out.println("Inserisci la parola e per chiudere tutto scrivere :q");
+            frase = scanner.nextLine();
+            if (frase.equals(":q")){
+                break;
+            }
+            for (int i=0;i<frase.length()-1;i++){
+                System.out.print(frase.charAt(i)+",");
+            }
+            System.out.println(frase.charAt(frase.length() - 1));
+        }
     }
 }
